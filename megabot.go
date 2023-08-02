b6472ba681472f055f578e24d6e276a0c11e79e1
Brian Welty <bkwelty@gmail.com>
2023-08-01 21:38:47 -0700

    Connect up receiving a Motors command over MQTT/protobuf

--- megabot.go
+++ megabot.go
@@ -13,34 +13,46 @@ import (
 	"log"
 	"os"
 	"time"
+	"google.golang.org/protobuf/proto"
 	"gobot.io/x/gobot/v2"
 	"gobot.io/x/gobot/v2/drivers/gpio"
 	"gobot.io/x/gobot/v2/platforms/mqtt"
 	"gobot.io/x/gobot/v2/platforms/sparki"
+	"github.com/pacificbrian/megabot/control"
 )
 
 func main() {
 	var connections []gobot.Connection
+	useRobot := false
 
-	mqttAdaptor := mqtt.NewAdaptor("tcp://0.0.0.0:1883", "megabotMQTT")
+	mqttAdaptor := mqtt.NewAdaptor("tcp://0.0.0.0:1883", "megabot-controller")
 	connections = append(connections, mqttAdaptor)
 
 	sparkiAdaptor := sparki.NewAdaptor(os.Args[1])
-	connections = append(connections, sparkiAdaptor)
 	led := gpio.NewLedDriver(sparkiAdaptor, "13")
+	if useRobot {
+		connections = append(connections, sparkiAdaptor)
+	}
 
 	work := func() {
 		selfPublish := false
 
-		mqttAdaptor.On("toggleLED", func(msg mqtt.Message) {
+		mqttAdaptor.On("ctrl/ToggleLED", func(msg mqtt.Message) {
 			log.Println(msg)
 			led.Toggle()
+		})
+
+		mqttAdaptor.On("ctrl/Motors", func(msg mqtt.Message) {
+			message := &control.MegabotCtrl{}
+			proto.Unmarshal(msg.Payload(), message);
+			log.Println("MQTT [Motors] Payload: ", message)
+			sparkiAdaptor.Move(message.Fvalue1, message.Fvalue2, -1.0)
 		})
 
 		if selfPublish {
 			gobot.Every(3*time.Second, func() {
 				brightness := byte(100)
-				mqttAdaptor.Publish("toggleLED",
+				mqttAdaptor.Publish("ctrl/ToggleLED",
 						    []byte{brightness})
 			})
 		}
